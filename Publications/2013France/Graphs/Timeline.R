7e3a563771a511a8107767c3fdefacb9cd1f955a
Will Beasley <wibeasley@hotmail.com>
2013-06-20 17:36:04 -0500

    Updating timeline graphs
    uses black arrows

--- Publications/2013France/Graphs/Timeline.R
+++ Publications/2013France/Graphs/Timeline.R
@@ -2,8 +2,9 @@ rm(list=ls(all=TRUE)) #Clear the memory for any variables set from any previous 
 require(ggplot2)
 require(grid)
 
-inputPath <- "F:/Users/wibeasley/Documents/Consulting/BGGrant2011/2013France/Graphs/Timeline.csv"
-outputPath <- "F:/Users/wibeasley/Documents/Consulting/BGGrant2011/2013France/Graphs/Timeline.png"
+inputPath <- "F:/Projects/Nls/NlsyLinksDetermination/Publications/2013France/Graphs/Timeline.csv"
+outputPath <- "F:/Projects/Nls/NlsyLinksDetermination/Publications/2013France/Graphs/Timeline.png"
+# outputPath <- "F:/Projects/Nls/NlsyLinksDetermination/Publications/2013France/Graphs/Timeline.emf"
 ds <- read.csv(inputPath, stringsAsFactors=T)
 
 width <- .2
@@ -13,24 +14,26 @@ ds$Height <- (ds$HeightMax + ds$HeightMin)/2
 maxMaxHeight <- max(ds$HeightMax)
 
 ds$Year <- (ds$StartYear + pmin(ds$StopYear, 2010))/2
-# ds
 
 # ds <- ds[ds$Cohort <= 1, ]
 ds <- ds[ds$Cohort <= 2, ]
 
-ds$Cohort <- factor(ds$Cohort, levels=1:2, labels=c("Gen1", "Gen2"))
+# ds$Cohort <- factor(ds$Cohort, levels=1:2, labels=c("Gen1", "Gen2"))
 
-ggplot(ds, aes(xmin=StartYear, xmax=StopYear, ymin=HeightMin, ymax=HeightMax, fill=Label)) +
+g <- ggplot(ds, aes(xmin=StartYear, xmax=StopYear, ymin=HeightMin, ymax=HeightMax, fill=Label)) +
 #   annotate("rect", xmin=-Inf, xmax=Inf, ymin=c(1, 2, 3)-.3, ymax=c(1, 2, 3)+.3, fill="gray90") +
   annotate("rect", xmin=-Inf, xmax=Inf, ymin=c(1, 2)-.3, ymax=c(1, 2)+.3, fill="gray90") +
 #   annotate("rect", xmin=-Inf, xmax=Inf, ymin=c(1)-.3, ymax=c(1)+.3, fill="gray90") +
+  
+  
   annotate("rect", xmin=2010, xmax=2014, ymin=-Inf, ymax=Inf, fill="gray80") +
-#     geom_segment(aes(x=StartYear, xend=StopYear, y=Height, yend=Height, color=Label)) +
   
   geom_vline(x=2006, color=hcl(h=240), size=4) +
   annotate("text", x=2006.5, y=3, label="Explicit Items\nFirst Asked\nin 2006", color=hcl(h=240, c=55, l=55), hjust=0.5, size=7, lineheight=.8) +
   
-  geom_rect(color=NA) + 
+  geom_rect(color=NA) + #The color bars of data
+  
+  #Arrows showing censored
   annotate("polygon", x=c(2010, 2010, 2012, 2010), y=c(1.1, .9, 1, 1.1)) +
   annotate("polygon", x=c(2010, 2010, 2012, 2010), y=c(1.1, .9, 1, 1.1)+1) +
   annotate("polygon", x=c(2010, 2010, 2012, 2010), y=c(1.1, .9, 1, 1.1)+1.2) +
@@ -57,4 +60,5 @@ ggplot(ds, aes(xmin=StartYear, xmax=StopYear, ymin=HeightMin, ymax=HeightMax, fi
   theme(axis.text.x=element_text(size=14)) +
   theme(axis.text.y=element_text(size=14, hjust=.5)) +
   theme(axis.ticks.length = unit(0, "cm")) 
-# ggsave(filename=outputPath)
+
+ggsave(filename=outputPath, g, width=10, height=6, dpi=600 )
