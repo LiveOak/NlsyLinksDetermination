8477d791f2b6732853bec5fe7bcc41ee76d5fff5
Will Beasley <wibeasley@hotmail.com>
2013-10-23 17:33:12 -0500

    Narrowed down proportion to same generation (not much difference)
    Ruled out using this item because too few of the remaining missing pairs
    are also missing responses to it
    
    R27379.00 CRES-1 LIVED WITH BOTH BIOLOGICAL PARENTS FROM BIRTH TO AGE
    18?

--- MicroscopicViews/VersionComparison/ArchiveProportionLinked.R
+++ MicroscopicViews/VersionComparison/ArchiveProportionLinked.R
@@ -4,6 +4,7 @@ require(scales)
 require(ggplot2)
 require(plyr)
 require(colorspace)
+require(NlsyLinks)
 includedRelationshipPaths <- c(2)
 # includedRelationshipPaths <- c(1)
 
@@ -16,7 +17,7 @@ oName_1 <- paste0(oName, "_1")
 oName_2 <- paste0(oName, "_2")
 
 startTime <- Sys.time()
-sql <- "SELECT  [AlgorithmVersion],[Subject1Tag],[Subject2Tag],[RFull]FROM [NlsLinks].[Process].[tblRelatedValuesArchive]"
+sql <- "SELECT  [AlgorithmVersion],[Subject1Tag], [Subject2Tag], [SameGeneration], [RFull] FROM [NlsLinks].[Process].[tblRelatedValuesArchive]"
 channel <- RODBC::odbcDriverConnect("driver={SQL Server};Server=Bee\\Bass; Database=NlsLinks; Uid=NlsyReadWrite; Pwd=nophi")
 odbcGetInfo(channel)
 ds <- sqlQuery(channel, sql, stringsAsFactors=F)
@@ -26,27 +27,35 @@ odbcCloseAll()
 # ds <- plyr::rbind.fill(ds, dsArchive)
 
 versionNumbers <- sort(unique(ds$AlgorithmVersion))
-dsProportionLinked <- data.frame(Version=versionNumbers, OfTotal=NA_integer_, OfDV=NA_integer_)
+dsProportionLinked <- data.frame(Version=versionNumbers, OfTotal=NA_integer_, OfDV=NA_integer_, ofDVSameGeneration=NA_integer_)
 
 dsOutcomes <- read.csv(file="./LinksForDistribution/Outcomes/ExtraOutcomes79.csv", stringsAsFactors=F)
 
 for( versionNumber in versionNumbers ) {
   dsSlice <- ds[ds$AlgorithmVersion==versionNumber, ]  
   dsSlice$R <- dsSlice$RFull
-  dsLink <- CreatePairLinksSingleEntered(outcomeDataset=dsOutcomes, linksPairDataset=dsSlice, linksNames="RFull", outcomeNames=oName)
+  linksNames <- c("RFull", "SameGeneration")
+  dsLink <- CreatePairLinksSingleEntered(outcomeDataset=dsOutcomes, linksPairDataset=dsSlice, linksNames=linksNames, outcomeNames=oName)
+  
   ofTotal <- mean(!is.na(dsLink$RFull)) 
   ofDV <- mean(!is.na(dsLink[!is.na(dsLink[, oName_1]) & !is.na(dsLink[, oName_2]), "RFull"])) 
+  ofDVSameGeneration <- mean(!is.na(dsLink[!is.na(dsLink[, oName_1]) & !is.na(dsLink[, oName_2]) & !is.na(dsLink$SameGeneration) & dsLink$SameGeneration!=0, "RFull"])) 
   
-  dsProportionLinked[dsProportionLinked$Version==versionNumber, c("OfTotal", "OfDV")] <- c(ofTotal, ofDV)
+  dsProportionLinked[dsProportionLinked$Version==versionNumber, c("OfTotal", "OfDV", "ofDVSameGeneration")] <- c(ofTotal, ofDV, ofDVSameGeneration)
 }
 (elapsed <- Sys.time() - startTime)
 
+
+# subset(dsSlice, select = c("Subject1Tag", "Subject2Tag", linksNames))
+
+
 g <- ggplot(dsProportionLinked, aes(x=Version)) +
-  geom_line(aes(y=OfDV),  color="blue") +
+  geom_line(aes(y=ofDVSameGeneration),  color="blue") +
+  geom_line(aes(y=OfDV),  color="purple") +
   geom_line(aes(y=OfTotal), color="black") +
   scale_y_continuous(name="Percent Linked", labels=percent) +
 #   xlab("Disagreement (Implicit vs Explicit)") +
   coord_cartesian(ylim=c(.85, 1)) + 
   theme_bw() + theme(legend.position = "none") 
-g
+# g
 ggsave(filename="./MicroscopicViews/VersionComparison/ProportionLinked.png", plot=g)
