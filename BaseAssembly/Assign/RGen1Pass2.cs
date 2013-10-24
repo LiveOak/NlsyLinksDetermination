b4610930f2f2a87a1b6a71458d4bb15f971d300a
Will Beasley <wibeasley@hotmail.com>
2013-10-24 01:05:20 -0500

    V82: Failed attempt to bring back part of V74 Uses parents' birthyear at 0, 5, 15, +; disconfirms; unlikely

--- BaseAssembly/Assign/RGen1Pass2.cs
+++ BaseAssembly/Assign/RGen1Pass2.cs
@@ -240,10 +240,10 @@ namespace Nls.BaseAssembly.Assign {
                 return Tristate.No;
             //else if ( birthYearAskedIn1987 == MarkerEvidence.Supports )
             //   return Tristate.Yes;
-            //else if ( birthYearAskedIn1988 == MarkerEvidence.Unlikely )
-            //   return Tristate.No;
-            //else if ( birthYearAskedIn1987 == MarkerEvidence.Unlikely )
-            //   return Tristate.No;
+            else if( birthYearAskedIn1988 == MarkerEvidence.Unlikely )
+                return Tristate.No;
+            else if( birthYearAskedIn1987 == MarkerEvidence.Unlikely )
+                return Tristate.No;
             else 
                 return Tristate.DoNotKnow;
         }
