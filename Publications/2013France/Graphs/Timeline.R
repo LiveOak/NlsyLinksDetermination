795d055c301a99c935bc4e84e888e5d7ca2d6f39
Will Beasley <wibeasley@hotmail.com>
2014-02-26 09:35:49 -0600

    Adding International Innovation article

--- Publications/2013France/Graphs/Timeline.R
+++ Publications/2013France/Graphs/Timeline.R
@@ -2,9 +2,9 @@ rm(list=ls(all=TRUE)) #Clear the memory for any variables set from any previous 
 require(ggplot2)
 require(grid)
 
-inputPath <- "F:/Projects/Nls/NlsyLinksDetermination/Publications/2013France/Graphs/Timeline.csv"
-outputPath <- "F:/Projects/Nls/NlsyLinksDetermination/Publications/2013France/Graphs/Timeline.png"
-# outputPath <- "F:/Projects/Nls/NlsyLinksDetermination/Publications/2013France/Graphs/Timeline.emf"
+inputPath <- "./Publications/2013France/Graphs/Timeline.csv"
+outputPath <- "./Publications/2013France/Graphs/Timeline.png"
+# outputPath <- "./Publications/2013France/Graphs/Timeline.emf"
 ds <- read.csv(inputPath, stringsAsFactors=T)
 
 width <- .2
@@ -16,20 +16,19 @@ maxMaxHeight <- max(ds$HeightMax)
 ds$Year <- (ds$StartYear + pmin(ds$StopYear, 2010))/2
 
 # ds <- ds[ds$Cohort <= 1, ]
-ds <- ds[ds$Cohort <= 2, ]
+# ds <- ds[ds$Cohort <= 2, ]
 
 # ds$Cohort <- factor(ds$Cohort, levels=1:2, labels=c("Gen1", "Gen2"))
 
 g <- ggplot(ds, aes(xmin=StartYear, xmax=StopYear, ymin=HeightMin, ymax=HeightMax, fill=Label)) +
-#   annotate("rect", xmin=-Inf, xmax=Inf, ymin=c(1, 2, 3)-.3, ymax=c(1, 2, 3)+.3, fill="gray90") +
-  annotate("rect", xmin=-Inf, xmax=Inf, ymin=c(1, 2)-.3, ymax=c(1, 2)+.3, fill="gray90") +
+  annotate("rect", xmin=-Inf, xmax=Inf, ymin=c(1, 2, 3)-.3, ymax=c(1, 2, 3)+.3, fill="gray90") +
+#   annotate("rect", xmin=-Inf, xmax=Inf, ymin=c(1, 2)-.3, ymax=c(1, 2)+.3, fill="gray90") +
 #   annotate("rect", xmin=-Inf, xmax=Inf, ymin=c(1)-.3, ymax=c(1)+.3, fill="gray90") +
-  
   
   annotate("rect", xmin=2010, xmax=2014, ymin=-Inf, ymax=Inf, fill="gray80") +
   
   geom_vline(x=2006, color=hcl(h=240), size=4) +
-  annotate("text", x=2006.5, y=3, label="Explicit Items\nFirst Asked\nin 2006", color=hcl(h=240, c=55, l=55), hjust=0.5, size=7, lineheight=.8) +
+#   annotate("text", x=2006.5, y=3, label="Explicit Items\nFirst Asked\nin 2006", color=hcl(h=240, c=55, l=55), hjust=0.5, size=7, lineheight=.8) +
   
   geom_rect(color=NA) + #The color bars of data
   
@@ -37,15 +36,15 @@ g <- ggplot(ds, aes(xmin=StartYear, xmax=StopYear, ymin=HeightMin, ymax=HeightMa
   annotate("polygon", x=c(2010, 2010, 2012, 2010), y=c(1.1, .9, 1, 1.1)) +
   annotate("polygon", x=c(2010, 2010, 2012, 2010), y=c(1.1, .9, 1, 1.1)+1) +
   annotate("polygon", x=c(2010, 2010, 2012, 2010), y=c(1.1, .9, 1, 1.1)+1.2) +
-#   annotate("polygon", x=c(2010, 2010, 2012, 2010), y=c(1.1, .9, 1, 1.1)+2) +
-#   annotate("polygon", x=c(2010, 2010, 2012, 2010), y=c(1.1, .9, 1, 1.1)+2.2) +
-  
-
-  
+  annotate("polygon", x=c(2010, 2010, 2012, 2010), y=c(1.1, .9, 1, 1.1)+2) +
+  annotate("polygon", x=c(2010, 2010, 2012, 2010), y=c(1.1, .9, 1, 1.1)+2.2) +
+    
   geom_text(aes(x=Year, y=Height, label=Label), hjust=.5) +
+  
 #   scale_y_discrete(labels=c("", "Nlsy79\n(Generation 1)", "", "", "")) + #, limits=c(1, 4)) +
-  scale_y_discrete(labels=c("", "Nlsy79\n(Generation 1)", "Nlsy79\n(Generation 2)", "", "")) + #, limits=c(1, 4)) +
-#   scale_y_discrete(labels=c("", "Nlsy79\n(Generation 1)", "Nlsy79\n(Generation 2)", "Nlsy97", "")) + #, limits=c(1, 4)) +
+#   scale_y_discrete(labels=c("", "Nlsy79\n(Generation 1)", "Nlsy79\n(Generation 2)", "", "")) + #, limits=c(1, 4)) +
+  scale_y_discrete(labels=c("", "Nlsy79\n(Generation 1)", "Nlsy79\n(Generation 2)", "Nlsy97", "")) + #, limits=c(1, 4)) +
+  
   scale_fill_brewer(palette="Set2", limits=c("Fertility", "Surveys", "DOB")) +
   #coord_cartesian(ylim=c(1-2*width, maxMaxHeight + 1*width)) +
   coord_cartesian(xlim=c(1954, 2014), ylim=c(1-2*width, maxMaxHeight + 1*width)) +
