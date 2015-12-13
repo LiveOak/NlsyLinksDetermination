b2975e4ce717feaf176de21f54764c5bbb933510
Will Beasley <wibeasley@hotmail.com>
2015-12-12 21:15:29 -0600

    version in outcomes name

--- ForDistribution/Outcomes/OutcomesAssembly.R
+++ ForDistribution/Outcomes/OutcomesAssembly.R
@@ -1,16 +1,20 @@
-# library(RODBC)
-library(plyr)
+library(RODBC)
+requireNamespace("plyr")
 # library(lubridate)
 rm(list=ls(all=TRUE))
 
+channel <- RODBC::odbcDriverConnect("driver={SQL Server}; Server=Bee\\Bass; Database=NlsLinks; Uid=NlsyReadWrite; Pwd=nophi")
+algorithmVersion <- max(sqlQuery(channel, "SELECT MAX(AlgorithmVersion) as AlgorithmVersion  FROM [NlsLinks].[Process].[tblRelatedValuesArchive]"))
+odbcClose(channel)
+
 pathInputGen1 <- "./ForDistribution/Outcomes/OutcomesGen1.csv"
 pathInputGen2 <- "./ForDistribution/Outcomes/OutcomesGen2.csv"
-pathOutput <- "./ForDistribution/Outcomes/ExtraOutcomes79.csv"
+pathOutput <-  sprintf("./ForDistribution/Outcomes/ExtraOutcomes79V%d.csv", algorithmVersion)
 
 dsGen1 <- read.csv(pathInputGen1, stringsAsFactors=FALSE)
 dsGen2 <- read.csv(pathInputGen2, stringsAsFactors=FALSE)
 
-ds <- rbind.fill(dsGen1, dsGen2)
+ds <- plyr::rbind.fill(dsGen1, dsGen2)
 
 length(ds$HeightZGenderAge)
 
